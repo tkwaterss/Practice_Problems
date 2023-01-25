fca57b34f1911639716c87d4dd2e90ea1df79284
Tobin Waters <tkwaterss@gmail.com>
2023-01-24 18:06:54 -0700

    Submitting first solution, second is still work in progress

--- alphabet.js
+++ alphabet.js
@@ -2,22 +2,23 @@
 // its letters in alphabetal order. assume there will be no punctuation or
 // numbers. example: `alphabet('hello')` // => 'ehllo'
 
-//First Solution
-// const alphabet = function(string) {
-//     return string.split("").sort().join('');
-// }
-// console.log(alphabet("hello"));
-
+First Solution
 const alphabet = function(string) {
-    let array = string.split('');
-    for (i = 0; i < array.length; i++) {
-        for (j = 0; j < array.length; j++) {
-            if (array[j] < array[i]) {
-                array.splice(j,1).unshift(j);
-            }
-        }
-    }
-    return array.join('');
+    return string.split("").sort().join('');
 }
-
 console.log(alphabet("hello"));
+
+//Unfinished alternate solution (issue is .slice or .splice)
+// const alphabet = function(string) {
+//     let array = string.split('');
+//     for (i = 0; i < array.length; i++) {
+//         for (j = 0; j < array.length; j++) {
+//             if (array[j] < array[i]) {
+//                 array.slice(j,1).unshift(j);
+//             }
+//         }
+//     }
+//     return array.join('');
+// }
+
+// console.log(alphabet("hello"));
